3683e820516445f1f4123ad116414539964c3261
limiao <1614824097@qq.com>
2019-07-20 23:57:27 +0800

    新加测试类

---
+++ src/main/java/cn/li/test/Test.java
@@ -0,0 +1,15 @@
+package cn.li.test;
+
+/**
+ * ClassName:Test
+ * Package:cn.li.test
+ * Description:
+ *
+ * @date:2019/7/20 23:56
+ * @author:cn.li
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是测试类");
+    }
+}
